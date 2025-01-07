ec8711f677f59a5d50e8e362161e094601f8f4c7
Sasikd <tkrajasasikumar@gmail.com>
2025-01-07 09:26:58 +0530

    Day 1

--- script/Vowels/script.js
+++ script/Vowels/script.js
@@ -1,7 +1,7 @@
 let str = "sasikumar";
 let count = 0;
 for (let i = 0; i < str.length; i++) {
-    if ( str[i] === 'a' || str[i] === 'e' || str[i] === 'i' || str[i] === 'o' || str[i] === 'u' ){
+    if ( str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' ){
         count++;
     }
 }
